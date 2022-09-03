e0654a0fd1cb74ff46b4670db2418b5e78185e65
Abhineet <Abhineet@DESKTOP-QLOK43Q>
2022-09-03 09:10:59 +0100

    First Commit

---
+++ src/Test1.java
@@ -0,0 +1,14 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Test1 ob = new Test1();
+		ob.display();
+	}
+
+	public void display(){
+		System.out.println("Heello");
+	}
+}
